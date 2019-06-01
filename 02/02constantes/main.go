f32a874b6efca69b0ec84dac86a2747a6a3364a5
Christian Rondo <crislord09@gmail.com>
2019-06-01 01:22:05 -0400

    Se realizo la clase 2 de tipos de datos en Go

---
+++ 02/02constantes/main.go
@@ -0,0 +1,40 @@
+//comenatrios de linea
+
+package main
+
+import "fmt"
+
+func main() {
+	//const nombre = "Pedro"
+
+	//fmt.Println(nombre)
+	/*
+		var a int
+		var b int8
+		n := "Pedro"
+		p := "Gómez"
+
+		a = 121212
+		b = 5
+
+		//casting
+		c := a + int(b)
+
+		fmt.Println(c)
+		fmt.Printf("hola %s %s %d ¿cómo estas? \n", n, p, b)
+		fmt.Printf("C es de tipo: %T\n", c)
+
+		//prioridad aritmética
+		// + - * /
+		//modulo es el residuo de la division
+		// () % * / + -
+		d := 6 + 6*(6-6)
+		fmt.Println(d)*/
+
+	//valor cero
+	var nombre string
+	var numero int
+	var entiendes bool
+	fmt.Println(nombre, numero, entiendes)
+
+}
