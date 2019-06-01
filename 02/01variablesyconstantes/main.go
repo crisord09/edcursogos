208df4e675ea341422eeca32e5d6035e20f6c5f5
Christian Rondo <crislord09@gmail.com>
2019-05-31 23:53:23 -0400

    Clase parte 2 variables

---
+++ 02/01variablesyconstantes/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	nombre, apellidos, edad := "Christian", "Rondo", 20
+
+	nombre, mascota := "pedrito", "Ñoja"
+
+	fmt.Println(nombre, apellidos, edad, mascota)
+}
