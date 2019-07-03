31e77f6b4274fcff7752d8a266a9d95872ddced2
Christian Rondo <crislord09@gmail.com>
2019-07-03 18:59:47 -0400

    Se modifico el fragmento 1 de la clase 6

--- 06/01funciones/main.go
+++ 06/01funciones/main.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	saludar()
+	saludar("Christian", 40)
 }
 
-func saludar() {
-	fmt.Println("HOLA MUNDO")
+func saludar(nombre string, edad uint8) {
+
+	if edad > 30 {
+		fmt.Println("HOLA", nombre, "Tienes esta edad putito", edad)
+	} else {
+		fmt.Println("HOLA", nombre, "eres menor de edad trasero")
+	}
+
 }
