43de44447cb36242127df6cfaa29d137f9b04676
Christian Rondo <crislord09@gmail.com>
2020-03-10 18:56:16 -0400

    se agrego comentarios e indicaciones mas entendibles a la clase 2

--- 02/02constantes/main.go
+++ 02/02constantes/main.go
@@ -1,4 +1,10 @@
-//comenatrios de linea
+//comenatrios de linea para documentar en codigo
+/*COMENTARIOS
+DE
+BLOQUE
+*/
+//Una constante no puede cambiar de dato en toda la app
+//
 
 package main
 
@@ -8,33 +14,37 @@ func main() {
 	//const nombre = "Pedro"
 
 	//fmt.Println(nombre)
-	/*
-		var a int
-		var b int8
-		n := "Pedro"
-		p := "Gómez"
 
-		a = 121212
-		b = 5
+	var a int
+	var b int8
+	n := "Pedro"
+	p := "Gómez"
 
-		//casting
-		c := a + int(b)
+	a = 121212
+	b = 5
 
-		fmt.Println(c)
-		fmt.Printf("hola %s %s %d ¿cómo estas? \n", n, p, b)
-		fmt.Printf("C es de tipo: %T\n", c)
+	/*SE REALIZA POR QUE 
+	casting int() poder realizar el cambio temporal de una variable para realizar una funcion especifica*/
+	c := a + int(b)
 
-		//prioridad aritmética
-		// + - * /
-		//modulo es el residuo de la division
-		// () % * / + -
-		d := 6 + 6*(6-6)
-		fmt.Println(d)*/
+	/* Printf le decimos que estamos enviando un dato string permite mostrar el formato que quere
+	*/
+	fmt.Println(c)
+	fmt.Printf("hola %s %s %d ¿cómo estas? \n", n, p, b)
+	/*%T nos ayuda a mostrar el tipo de valor de una variable*/
+	fmt.Printf("C es de tipo: %T\n", c)
 
-	//valor cero
-	var nombre string
-	var numero int
-	var entiendes bool
-	fmt.Println(nombre, numero, entiendes)
+	//prioridad aritmética
+	// * / + -
+	//modulo es el residuo de la division entre 2 elementos
+	// () % * / + -
+	d := 6 + 6*(6-6)
+	fmt.Println(d)
+
+	// Anque yo no les haya dado un valor a una variable todas comienzan en cero
+	//var nombre string
+	//var numero int
+	//var entiendes bool
+	//fmt.Println(nombre, numero, entiendes)
 
 }
--- 05/02forcontinuo/main.go
+++ 05/02forcontinuo/main.go
@@ -1,11 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 
-	/*a := 5
+	a := 5
 	for a > 0 {
 		a--
 		fmt.Println(a)
-	}*/
+	}
 
 }
